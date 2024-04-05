79fa00c52174b68c7445cd8ed13ecf2656e4c370
ucs98 <Umesh@9621>
2024-04-06 02:02:23 +0530

    bug-fixed

--- src/Test.java
+++ src/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("I am in feature first");
         System.out.println("commit in master branch");
+        System.out.println("I have fixed the bugs");
     }
 }
