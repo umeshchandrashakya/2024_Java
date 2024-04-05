a869fd80f2592cc97a76c293cdf3cc4332cea295
ucs98 <Umesh@9621>
2024-04-06 01:30:32 +0530

    commiting in master branch

--- src/Test.java
+++ src/Test.java
@@ -4,5 +4,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("I am in feature first");
+        System.out.println("commit in master branch");
     }
 }
